90cb54dd5e091dbc222efcd346984f1c30d7992e
VikingHesse <viking.hesse@elev.ga.ntig.se>
2021-05-13 22:59:41 +0200

    Update försök.py

--- försök.py
+++ försök.py
@@ -1,5 +1,6 @@
 import plotly.express as px 
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd 
@@ -8,16 +9,19 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
-# genererar data
+#genererar data
 genderdata = pd.read_csv("Gender_Data.csv", encoding="ISO-8859-1", header=0)
 dagligadödsfall = pd.read_csv("National_Daily_Deaths.csv", encoding="ISO-8859-1", header=0)
 veckodata = pd.read_csv("Municipality_Weekly_Data.csv", encoding="UTF-8", header=0)
 gbgdata = veckodata[veckodata["Municipality"].str.contains("Göteborg")]
+åldersdata = pd.read_csv("National_Total_Deaths_by_Age_Group.csv", encoding="UTF-8", header=0)
 
-# skapa figs
-fig = px.pie(genderdata, values='Total_Deaths', names='Gender', title='Antalet dödsfall män kontra kvinnor')
-fig2 = px.bar(dagligadödsfall, x="Date", y="National_Daily_Deaths", title="Antalet döda i COVID per dag")
+#skapa figs
+fig = px.pie(genderdata, values='Total_Deaths', names='Gender', title='Antalet dödsfall män kontra kvinnor', height=700)
+fig2 = px.bar(dagligadödsfall, x="Date", y="National_Daily_Deaths", title="Antalet döda i COVID per dag", height=700)
 fig3 = px.bar(gbgdata, x="Municipality", y="Weekly_Cases_per_100k_Pop", color="Municipality", animation_frame="Week_Number", animation_group="Municipality", range_y=[0,75], title="Antalet fall i Göteborg per 100k veckorna 1-53 2020", height=700)
+
+åldersdata_graf = px.pie(åldersdata, values="Total_Cases", names="Age_Group", title="Totala fall", height=700)
 
 #flyttar slidern på fig 3
 fig3['layout']['sliders'][0]['pad']=dict(r= 10, t= 150,)
@@ -54,8 +58,38 @@ app.layout = html.Div(children=[
             figure=fig3,
         ),
     ]),
+
+    html.Div([
+        html.H1(children="COVID DATA GRAF 4"),
+
+        dcc.Dropdown(
+            id = "drop",
+            options = [dict(label = "Totala fall", value="Totala fall"), dict(label = "Totala dödsfall", value="Totala dödsfall"), dict(label = "Totala IVA fall", value="Totala IVA fall")],
+            value = "totalfall"
+        ),
+        
+        dcc.Graph(
+            id="åldersgraf",
+            figure=åldersdata_graf,
+        ),
+
+    ]),
 ])
 
+@app.callback(
+    Output("åldersgraf", "figure"),
+    [Input("drop", "value")]
+)
+def update_figure(value):
+    global värden
+
+    if value == "Totala fall":  värden = "Total_Cases"
+    elif value == "Totala dödsfall": värden = "Total_Deaths"
+    elif value == "Totala IVA fall": värden = "Total_ICU_Admissions"
+
+    åldersdata_graf = px.pie(åldersdata, values=värden, names="Age_Group", title=f"{value}")
+    åldersdata_graf.update_layout(transition_duration=500)
+    return åldersdata_graf
 
 
 
