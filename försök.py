93a6dc743018a297db5b8e74fb667b97f47addc6
VikingHesse <viking.hesse@elev.ga.ntig.se>
2021-05-14 00:37:48 +0200

    Update försök.py

--- försök.py
+++ försök.py
@@ -15,14 +15,16 @@ dagligadödsfall = pd.read_csv("National_Daily_Deaths.csv", encoding="ISO-8859-1
 veckodata = pd.read_csv("Municipality_Weekly_Data.csv", encoding="UTF-8", header=0)
 gbgdata = veckodata[veckodata["Municipality"].str.contains("Göteborg")]
 åldersdata = pd.read_csv("National_Total_Deaths_by_Age_Group.csv", encoding="UTF-8", header=0)
-regiondata = pd.read_csv("Regional_Daily_Cases.csv", encoding="UTF-8", header=0)
+regiondailydata = pd.read_csv("Regional_Daily_Cases.csv", encoding="UTF-8", header=0)
+regiontotaldata = pd.read_csv("Regional_Totals_Data.csv", encoding="UTF-8", header=0)
 
 #skapa figs
 genderdata_graf = px.pie(genderdata, values='Total_Deaths', names='Gender', title='Antalet dödsfall män kontra kvinnor', height=700)
 dagligadödsfall_graf = px.bar(dagligadödsfall, x="Date", y="National_Daily_Deaths", title="Antalet döda i COVID per dag", height=700)
 gbgdata_graf = px.bar(gbgdata, x="Municipality", y="Weekly_Cases_per_100k_Pop", color="Municipality", animation_frame="Week_Number", animation_group="Municipality", range_y=[0,75], title="Antalet fall i Göteborg per 100k veckorna 1-53 2020", height=700)
 åldersdata_graf = px.pie(åldersdata, values="Total_Cases", names="Age_Group", title="Totala fall", height=700)
-regiondata_graf = px.line(regiondata, x="Date", y="Västra_Götaland")
+regiondata_graf = px.line(regiondailydata, x="Date", y="Västra_Götaland", title="Antalet döda i COVID per dag i Västra Götaland", height=700)
+regiontotaldata_graf = px.scatter(regiontotaldata, x="Total_Deaths", y="Total_ICU_Admissions", color="Region", size="Total_Cases",title="Totala dödsfall, IVA fall för varje region", height=700)
 
 #flyttar slidern på göteborgsgrafen
 gbgdata_graf['layout']['sliders'][0]['pad']=dict(r= 10, t= 150,)
@@ -82,6 +84,16 @@ app.layout = html.Div(children=[
         dcc.Graph(
             id="regiongraf",
             figure=regiondata_graf,
+        ),
+    ]),
+
+    html.Div([
+        html.H1(children="COVID DATA GRAF 5"),
+
+
+        dcc.Graph(
+            id="regiontotaldatagraf",
+            figure=regiontotaldata_graf,
         ),
     ]),
 
